e992da37a6f764044bb96abf3bb8a32c17840967
William S <will107673@gmail.com>
2026-09-02 16:28:38 -0500

    Entity Mascota

--- src/main/java/com/example/veterinaria/Entity/Mascota.java
+++ src/main/java/com/example/veterinaria/Entity/Mascota.java
@@ -1,4 +1,15 @@
 package com.example.veterinaria.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Mascotas")
 public class Mascota {
+    private Long id;
+    private String nombre;
+    private String especie;
+    private String raza;
+    private Integer edad;
+    private double peso;
 }

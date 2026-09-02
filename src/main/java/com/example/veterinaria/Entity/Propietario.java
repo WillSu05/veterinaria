cf1d0d6c7417d777e9b0c2cf096eed38ad6f80e2
William S <will107673@gmail.com>
2026-09-02 16:21:12 -0500

    Entity Propietario

--- src/main/java/com/example/veterinaria/Entity/Propietario.java
+++ src/main/java/com/example/veterinaria/Entity/Propietario.java
@@ -1,13 +1,21 @@
 package com.example.veterinaria.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "propietarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Propietario {
+    @Id
     private Long id;
     private String nombre;
     private String documento;
-    private String telefono; 
+    private String telefono;
 }
